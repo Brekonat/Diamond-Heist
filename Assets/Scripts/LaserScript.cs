84f79a454bd033f8afc5e704c46673f5e70995fc
Joseph <joseph.galvin@blueyonder.co.uk>
2022-01-30 11:43:34 +0000

    Added new laser
    New laser

--- Assets/Scripts/LaserScript.cs
+++ Assets/Scripts/LaserScript.cs
@@ -4,12 +4,26 @@ using UnityEngine;
 
 public class LaserScript : MonoBehaviour
 {
+    [SerializeField] private Rigidbody2D laserrb;
+
     // Start is called before the first frame update
     void Start()
     {
-        Physics2D.IgnoreLayerCollision(7,8);
+        laserrb.velocity = new Vector2(-4, 0);
+        Destroy(gameObject, 1f);
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.CompareTag("player_big"))
+        {
+            Destroy(gameObject);
+        }
+        if (collision.collider.CompareTag("player_small"))
+        {
+            collision.gameObject.GetComponent<PlayerSmallMovement>().swappedPlayerSmall = false;
+        }
 
+    }
     // Update is called once per frame
     void Update()
     {
